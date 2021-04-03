fb1daffa5e89a5764e28306475f9f82d534f4ee2
alkeys <am18007@ues.edu.sv>
2021-04-03 09:02:04 -0600

    vercion 0.1.0

--- src/tarjetas_claro/Tarjetas_claro.java
+++ src/tarjetas_claro/Tarjetas_claro.java
@@ -1,6 +1,7 @@
 package tarjetas_claro;
 public class Tarjetas_claro {
     public static void main(String[] args) {
+        /*codigo echo sin fines de lucro solo para conocimiento general*/
 graficos xd=new graficos();
     }   
 }
